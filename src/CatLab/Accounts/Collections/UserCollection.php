cc5960af8c9317be98b9bc7905dd29d32bc19209
Thijs Van der Schaeghe <thijs@catlab.be>
2015-01-05 16:09:23 +0100

    Forcing int on user id.

---
+++ src/CatLab/Accounts/Collections/UserCollection.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: daedeloth
+ * Date: 5/01/15
+ * Time: 16:09
+ */
+
+namespace CatLab\Accounts\Collections;
+
+
+use Neuron\Collections\Collection;
+
+class UserCollection
+	extends Collection {
+
+
+
+}
