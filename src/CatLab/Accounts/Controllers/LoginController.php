27a6649a1ade62198063ad3cb01b60be6718b878
Thijs Van der Schaeghe <thijs@catlab.be>
2019-11-18 09:29:34 +0100

    Use lax samesite on first visit cookie.

--- src/CatLab/Accounts/Controllers/LoginController.php
+++ src/CatLab/Accounts/Controllers/LoginController.php
@@ -89,7 +89,7 @@ class LoginController
         if (!isset($cookies['fv'])) {
             $response = Response::redirect($this->module->getRoutePath () . '/register');
             //$response->setCookies(array( 'fv' => time() ));
-            setcookie('fv', time(), time() + 60*60*24*365*2);
+            setcookie('fv', time(), time() + 60*60*24*365*2, [ 'samesite' => 'Lax' ]);
             return $response;
         }
 
